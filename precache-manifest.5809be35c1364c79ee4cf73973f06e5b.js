6c6f2927cd3843fb31222d4bfef144b301560bb7
nastyakpi1995 <nastyakpi1995@gmail.com>
2020-08-22 15:19:07 +0300

    Updates

---
+++ precache-manifest.5809be35c1364c79ee4cf73973f06e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4a4cffb424b5a91a2b83313aa3d1a5",
+    "url": "/yeal/index.html"
+  },
+  {
+    "revision": "7667582a076e8c1a922d",
+    "url": "/yeal/static/js/2.5258922d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yeal/static/js/2.5258922d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e52b7752fdb5d3f7bf4",
+    "url": "/yeal/static/js/main.b288a09e.chunk.js"
+  },
+  {
+    "revision": "c29844f31d3951e31700",
+    "url": "/yeal/static/js/runtime-main.3e90ab82.js"
+  }
+]);
